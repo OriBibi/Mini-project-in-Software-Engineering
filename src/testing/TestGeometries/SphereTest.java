72a3248d296c48431c853be6cde7b9d832ba7350
Golanm16 <Golanm16@gmail.com>
2019-05-12 13:42:43 +0300

    rnm

--- src/testing/TestGeometries/SphereTest.java
+++ src/testing/TestGeometries/SphereTest.java
@@ -64,4 +64,69 @@ class SphereTest {
         list2 = sphere2.findIntersections(ray2);
         assertEquals(answerList2, list2);
     }
+
+    @Test
+    public void testFindIntersections() {
+        /**
+         * test for p0==o
+         */
+        Sphere sphere = new Sphere(5, new Point3D(0, 0, 1));
+        Ray ray = new Ray( new Vector(0, 0, -2),new Point3D(0, 0, 1));
+        ArrayList<Point3D> list = new ArrayList<Point3D>();
+
+        list.add(new Point3D(0, 0, -4));
+        assertEquals(sphere.findIntersections(ray), list);
+
+        /**
+         * test for p0 inside the sphere with the same Direction
+         */
+
+        sphere = new Sphere(5, new Point3D(0, 0, 1));
+        ray = new Ray( new Vector(0, 0, 1),new Point3D(0, 0, 2));
+        list.clear();
+        list.add(new Point3D(0, 0, 6));
+        assertEquals(sphere.findIntersections(ray), list);
+
+        /**
+         * test for p0 inside the sphere with the different Direction
+         */
+
+        sphere = new Sphere(4,new  Point3D());
+        ray = new Ray( new Vector(1, 0, 0),new Point3D(0, 1, 0));
+        list = new ArrayList<Point3D>();
+        list.add(new Point3D(Math.sqrt(15), 1, 0));
+        assertEquals(sphere.findIntersections(ray), list);
+
+        /**
+         * Test for ray Parallel to the sphere without passing through it
+         */
+        sphere = new Sphere(1, new Point3D());
+        ray = new Ray(new Vector(1, 0, 0),new Point3D(0, 0, 2));
+        list = new ArrayList<Point3D>();
+        assertEquals(sphere.findIntersections(ray), list);
+
+        /**
+         * Test for getting 2 point on the sphere
+         */
+        ray = new Ray(new Vector(0, 0, -1),new Point3D(0, 0, 3));
+        list.add(new Point3D(0, 0, 1));
+        list.add(new Point3D(0, 0, -1));
+        assertEquals(sphere.findIntersections(ray), list);
+
+        /**
+         * test for ray that tangent to the sphere
+         */
+        ray = new Ray(new Vector(0, -5, 0),new Point3D(0, 1, 1));
+        list.clear();
+        list.add(new Point3D(0, 0, 1));
+        assertEquals(sphere.findIntersections(ray), list);
+
+        /**
+         * test for when the ray start on the surface of the sphere and go away from it
+         */
+        ray = new Ray( new Vector(0, 0, 1),new Point3D(0, 0, 1));
+        // System.out.println(sphere.findIntersections(ray));
+        list.clear();
+        assertEquals(sphere.findIntersections(ray), list);
+    }
 }
