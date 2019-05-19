f0e68657997d376111203f6a3177fc2bd7cb3e38
Golanm16 <Golanm16@gmail.com>
2019-05-19 13:37:57 +0300

    sunday morning

--- src/geometries/Geometry.java
+++ src/geometries/Geometry.java
@@ -12,4 +12,11 @@ public abstract class  Geometry {
     protected Color geoColor;
     public abstract List<Point3D> findIntersections( Ray ray);
 
+    public Color getGeoColor() {
+        return geoColor;
+    }
+
+    public void setGeoColor(Color geoColor) {
+        this.geoColor = geoColor;
+    }
 }
