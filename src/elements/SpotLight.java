18d7ba4ac0f0978bd5fa37de549d8e2aa429d2a0
Golanm16 <Golanm16@gmail.com>
2019-05-19 15:57:40 +0300

    lighht

--- src/elements/SpotLight.java
+++ src/elements/SpotLight.java
@@ -12,21 +12,29 @@ public class SpotLight extends PointLight {
     // ***************** Constructor ********************** //
     public SpotLight(Color color, Point3D position, Vector direction,
                      double kc, double kl, double kq){
-        this.color=color;
-        this.position=position;
-        this.direction=direction;
+        this.setColor(color);
+        this.setPosition(position);
+        this.setDirection(direction);
         this.setKc(kc);
         this.setKl(kl);
         this.setKq(kq);
     }
     public SpotLight(){
-        this.color=Color.yellow;
-        this.position=new Point3D(Coordinate.ZERO,Coordinate.ZERO,new Coordinate(10.0));
-        this.direction=new Vector(new Coordinate(1.0),Coordinate.ZERO,Coordinate.ZERO);
+        this.setColor(Color.yellow);
+        this.setPosition(new Point3D(Coordinate.ZERO,Coordinate.ZERO,new Coordinate(10.0)));
+        this.setDirection(new Vector(new Coordinate(1.0),Coordinate.ZERO,Coordinate.ZERO));
         this.setKc(0);
         this.setKl(0);
         this.setKq(0);
     }
     // ***************** Getters/Setters ********************** //
     public Color getIntensity(Point3D point);
+
+    public Vector getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Vector direction) {
+        this.direction = direction;
+    }
 }
