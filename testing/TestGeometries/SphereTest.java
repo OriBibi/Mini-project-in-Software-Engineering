82885a3139d53c1e01d36fae55fac5462909852e
Ori <oribibi10@gmail.com>
2019-06-02 11:49:57 +0300

    Ori changes Testing

--- testing/TestGeometries/SphereTest.java
+++ testing/TestGeometries/SphereTest.java
@@ -1,5 +1,6 @@
 package TestGeometries;
 
+import elements.Camera;
 import geometries.Sphere;
 import org.junit.jupiter.api.Test;
 import primitives.Point3D;
@@ -64,6 +65,61 @@ class SphereTest {
         assertEquals(answerList2, list2);
     }
 */
+@Test
+public void Test16(){
+
+    final int WIDTH  = 3;
+    final int HEIGHT = 3;
+
+    System.out.println("Test16: Sphere intersection test");
+
+    Ray[][] rays = new Ray [HEIGHT][WIDTH];
+
+    Camera camera = new Camera(new Point3D(0.0 ,0.0 ,0.0),
+            new Vector (0.0, 1.0, 0.0),
+            new Vector (0.0, 0.0, -1.0));
+
+    Sphere sphere1 = new Sphere(1, new Point3D(0.0, 0.0, -3.0));
+    Sphere sphere2 = new Sphere(10, new Point3D(0.0, 0.0, -3.0));
+
+    // Only the center ray intersect the sphere in two locations
+    List<Point3D> intersectionPointsSphere1 = new ArrayList<Point3D>();
+
+    // The sphere encapsulates the view plane - all rays intersect with the sphere once
+    List<Point3D> intersectionPointsSphere2 = new ArrayList<Point3D>();
+
+    System.out.println("Camera:\n" + camera);
+
+    for (int i = 0; i < HEIGHT; i++){
+        for (int j = 0; j < WIDTH; j++){
+
+            rays[i][j] = camera.constructRayThroughPixel(
+                    WIDTH, HEIGHT, j, i, 1, 3 * WIDTH, 3 * HEIGHT);
+
+            List<Point3D> rayIntersectionPoints1 = sphere1.findIntersections(rays[i][j]);
+            List<Point3D> rayIntersectionPoints2 = sphere2.findIntersections(rays[i][j]);
+
+            for (Point3D iPoint: rayIntersectionPoints1)
+                intersectionPointsSphere1.add(iPoint);
+
+            for (Point3D iPoint: rayIntersectionPoints2)
+                intersectionPointsSphere2.add(iPoint);
+
+        }
+    }
+
+    assertTrue(intersectionPointsSphere1.size() == 2);
+    assertTrue(intersectionPointsSphere2.size() == 9);
+
+    System.out.println("Intersection Points:");
+    for (Point3D iPoint: intersectionPointsSphere1)
+    {
+
+        assertTrue(iPoint.equals(new Point3D(0.0, 0.0, -2.0)) == 0 ||
+                iPoint.equals(new Point3D(0.0, 0.0, -4.0)) == 0);
+        System.out.println(iPoint);
+    }
+}
     @Test
     public void testFindIntersections() {
         /**
